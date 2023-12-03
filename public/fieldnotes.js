8bbc278657b73339d3e58927f9abd3d97b601237
Daniel Hartley <daniel.hartley@altruistiq.com>
2023-12-03 17:50:54 +0000

    xeno-canto help

--- public/fieldnotes.js
+++ public/fieldnotes.js
@@ -585,39 +585,7 @@ export const fieldnotes = [
           }
         ]
       },
-      {
-        id: "species-test-template",
-        name: "Species-card-tests",
-        parent: "grid-template",
-        pairedTemplateId: "species-template",
-        isTest: true,
-        targets: [
-          {
-            id: "common",
-            name: "common name"
-          },
-          {
-            id: "latin",
-            name: "latin name"
-          }
-        ],
-        score: 0,
-        scores: []
-      },
-      {
-        id: "species-template",
-        name: "Species-cards",
-        parent: "grid-template",
-        pairedTemplateId: "species-test-template",
-        isTest: false,
-        isTestable: true
-      },
-      {
-        id: "species-list-template",
-        name: "Species-list",
-        parent: "grid-template",
-        isTest: false
-      }
+      ...templates
     ]
   }
 ]
