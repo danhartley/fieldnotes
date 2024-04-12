852e2c1e5907ad66d3489c7feb70878194817b2b
Daniel Hartley <daniel.hartley@altruistiq.com>
2024-04-12 16:57:44 +0100

    add constants for locators

--- public/tests/read-fieldnotes/read-fieldnotes-tests.js
+++ public/tests/read-fieldnotes/read-fieldnotes-tests.js
@@ -1,42 +1,49 @@
 import puppeteer from 'puppeteer'
 
-const DELAY = 1000
-const FIELDNOTES = 'Streets of Lisbon, Portugal, Wed Feb 28 2024'
+const DELAY_FOR_TITLES = 1000
+const FIELDNOTES_TITLE = 'Streets of Lisbon, Portugal, Wed Feb 28 2024'
+const FIELDNOTES_TITLE_ID = '#fn-autocomplete-title-input-text'
+const FIELDNOTES_BTN_ID = '#fetch-fieldnotes-btn'
+const PRINT_FIELDNOTES_BTN_ID = '#print-fieldnotes-btn'
+const PRINT_FIELDNOTES_WITH_PAGE_BREAKS_BTN_ID = '#print-fieldnotes-with-page-breaks-btn'
 
 const readFieldnotes = async () => {
-  let browser, page, classList = null
+  let browser, page = null
   
   try {
-    // Launch the browser and open a new blank page
+    // Launch the browser
     browser = await puppeteer.launch({headless: false})
+    // Create a page
     page = await browser.newPage()
 
     await page.setViewport({ width: 1280, height: 1024 })
 
-    // Navigate the page to read fieldnotes
+    // Go to read fieldnotes page
     await page.goto('http://localhost:1234')  
     
     // Abort test if the fetch button is already enabled
-    const classList = await page.$eval('#fetch-fieldnotes-btn', el => el.classList)
+    const classList = await page.$eval(FIELDNOTES_BTN_ID, el => el.classList)
     if(isEnabled({classList})) {
       console.error('The initial state is wrong, aborting test')
     }
 
     // Populate fieldnotes to display
-    await page.locator('#fn-autocomplete-title-input-text').fill(FIELDNOTES) 
+    await page.locator(FIELDNOTES_TITLE_ID).fill(FIELDNOTES_TITLE) 
 
     setTimeout(async() => {
       // Trigger selection of the entered fieldnotes
       await page.keyboard.press('Enter')
 
       // Fetch fieldnotes if the fetch button is enabled
-      const classList = await page.$eval('#fetch-fieldnotes-btn', el => el.classList)
+      const classList = await page.$eval(FIELDNOTES_BTN_ID, el => el.classList)
       if(isEnabled({classList})) {
-        await page.click('#fetch-fieldnotes-btn')
+        await page.click(FIELDNOTES_BTN_ID)
       } else {
         await page.screenshot({path: './public/tests/read-fieldnotes/screenshots/fetch button not ready.png', fullPage: true})
       }              
-    }, DELAY)    
+    }, DELAY_FOR_TITLES)
+
+
   } catch (error) { 
     await scroll({page})
     await page.screenshot({path: './public/tests/read-fieldnotes/screenshots/error.png', fullPage: true})
@@ -45,7 +52,7 @@ const readFieldnotes = async () => {
   } finally {
     setTimeout(async() => {
       // await browser.close()
-  } , DELAY + 2000)
+  } , DELAY_FOR_TITLES + 2000)
   }
 }
 
@@ -103,8 +110,7 @@ const isEnabled = ({classList}) => {
 /**
  * helper classes
  * logger
- * snapshots for errors
- * error handling
- * suite of tests (how comprehensive?)
+ * suite of tests (how comprehensive?), how to run?
  * reset the database i.e. when creating entires such as user and field notes
+ * blog: what testing and why useful
  */
