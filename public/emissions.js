b841e2125daf5fb65c019e5040c813228d70dc72
Daniel Hartley <daniel.hartley@altruistiq.com>
2024-07-15 14:03:29 +0100

    browser emissions

--- public/emissions.js
+++ public/emissions.js
@@ -1,27 +1,17 @@
-import { registerServiceWorker, getNetworkTraffic, getEmissions } from '@danhartley/emissions'
+import { browser } from '@danhartley/emissions'
 
 export const getSiteEmissions = () => {
   (async () => {
-    registerServiceWorker()
+    await browser.registerServiceWorker()
   
     const domain = window.location.host
-    console.log(domain)
-    const traffic = await getNetworkTraffic({domain})
-    console.log(traffic)
+    const { pageWeight, count, greenHosting, emissions, mgCO2 } = await browser.getPageEmissions({domain})
+    
+    console.log(`Report for ${domain}`)
+    console.log('Page weight: ', `${pageWeight / 1000} Kbs`)
+    console.log('Requests ', count)  
+    console.log('Emissions: ', `${mgCO2} mg of CO2`)
+    console.log(greenHosting ? 'Hosting: green hosting' : 'Hosting: not green hosting')
 
-    if(traffic.responses.length) {
-        let bytes = traffic.responses.reduce((acc, curr) => acc + curr.responseBytes, 0)
-        console.log('kilobytes: ', bytes / 1000)
-        const emissions = await getEmissions({
-          bytes,
-          model: '1byte',
-          hostingOptions: {
-            domain
-          }
-        })
-
-        console.log('total emissions: ', `${emissions.emissions * 1000} mg of CO2`)
-        console.log(emissions.isGreen ? `${domain} is green hosted` : `${domain} is not green hosted`)
-    }
   })() 
 }
