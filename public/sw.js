c0b6b370f9634ef6912ea34eb2ba176658b73b37
Daniel Hartley <daniel.hartley@altruistiq.com>
2024-07-26 12:08:08 +0100

    config for deploying files for emissions

--- public/sw.js
+++ public/sw.js
@@ -1,37 +1,215 @@
-const getResponseDetails = async (response) => {
+// db constants
+
+const DB = 'emissionsDB';
+const STORE = 'emissions';
+const compressionRates = {
+  brotli: [
+    {
+      level: 0,
+      rate: 1.2,
+    },
+    {
+      level: 1,
+      rate: 2.1,
+    },
+    {
+      level: 2,
+      rate: 2.5,
+    },
+    {
+      level: 3,
+      rate: 3.0,
+    },
+    {
+      level: 4,
+      rate: 3.5,
+    },
+    {
+      level: 5,
+      rate: 4.0,
+    },
+    {
+      level: 6,
+      rate: 4.3,
+    },
+    {
+      level: 7,
+      rate: 4.6,
+    },
+    {
+      level: 8,
+      rate: 4.9,
+    },
+    {
+      level: 9,
+      rate: 5.0,
+    },
+    {
+      level: 10,
+      rate: 5.1,
+    },
+    {
+      level: 11,
+      rate: 5.2,
+    },
+  ],
+  gzip: [
+    {
+      level: 0,
+      rate: 1,
+    },
+    {
+      level: 1,
+      rate: 1.5,
+    },
+    {
+      level: 2,
+      rate: 2.0,
+    },
+    {
+      level: 3,
+      rate: 2.3,
+    },
+    {
+      level: 4,
+      rate: 2.5,
+    },
+    {
+      level: 5,
+      rate: 2.8,
+    },
+    {
+      level: 6,
+      rate: 3.0,
+    },
+    {
+      level: 7,
+      rate: 3.2,
+    },
+    {
+      level: 8,
+      rate: 3.3,
+    },
+    {
+      level: 9,
+      rate: 3.4,
+    },
+  ],
+};
+
+const getBytes = ({
+  compressedBytes,
+  uncompressedBytes,
+  encoding,
+  compressionOptions,
+}) => {
+  if (compressedBytes !== 0) return compressedBytes
+
+  return (
+    compressUncompressedBytes({
+      encoding,
+      bytes: uncompressedBytes,
+      compressionOptions,
+    }) || 0
+  )
+};
+
+const compressUncompressedBytes = ({
+  encoding,
+  bytes,
+  compressionOptions,
+}) => {
+  // default compression rates
+  let BR = compressionRates.brotli.find((b) => b.level === 3).rate;
+  let GZIP = compressionRates.gzip.find((g) => g.level === 5).rate;
+  let DEFLATE = 1; // tbd
+  let ZSTD = 1; // tbd
+
+  if (compressionOptions) {
+    BR =
+      compressionRates.brotli.find((b) => b.level === compressionOptions.br)
+        ?.rate || BR;
+    GZIP =
+      compressionRates.gzip.find((g) => g.level === compressionOptions.gzip)
+        ?.rate || GZIP;
+  }
+
+  let ratio;
+  switch (encoding) {
+    case 'br':
+      ratio = BR;
+      break
+    case 'gzip':
+      ratio = GZIP;
+      break
+    case 'deflate':
+      ratio = DEFLATE;
+      break
+    case 'zstd':
+      ratio = ZSTD;
+      break
+    default:
+      ratio = 1;
+  }
+
+  return Math.round(bytes / ratio)
+};
+
+const getResponseDetails = async (response, env, compressionOptions) => {
   const acceptedStatuses = [200, 304];
-  const status = response.status();    
+  const status = response.status ;
 
   if (!response || !acceptedStatuses.includes(status)) {
     return null
   }
 
-  const url = response.url();
+  const isBrowser = env === 'browser';
 
-  const buffer = await response.buffer();
+  const getHeader = (header) =>
+    response.headers.get(header)
+      ;
+  const getBuffer = async () =>
+    response.arrayBuffer() ;
+
+  const url = isBrowser ? response.url : response.url();
+  const contentLength = getHeader('Content-Length');
+  const contentType = getHeader('Content-Type');
+  const contentEncoding = getHeader('Content-Encoding') || 'n/a';
+  const buffer = await getBuffer();
+
   const uncompressedBytes = buffer.length;
-  const compressedContentLength = response.headers()['content-length'];
-  const compressedBytes = compressedContentLength
-    ? parseInt(compressedContentLength, 10)
-    : 0;
-  const contentType = response.headers()['content-type'];
-  const encoding = response.headers()['content-encoding'] || 'n/a';
-  const resourceType = response.request().resourceType();    
+  const compressedBytes = contentLength ? parseInt(contentLength, 10) : 0;
+  const bytes = getBytes({
+    compressedBytes,
+    uncompressedBytes,
+    encoding: contentEncoding,
+    compressionOptions,
+  });
+
+  let resourceType;
+
+  {
+    if (contentType.includes('text/html')) {
+      resourceType = 'document';
+    } else if (contentType.includes('application/javascript')) {
+      resourceType = 'script';
+    } else if (contentType.includes('image/')) {
+      resourceType = 'image';
+    } else {
+      resourceType = 'other';
+    }
+  }
 
   return {
-      url,
-      contentType,
-      compressedBytes,
-      uncompressedBytes,
-      encoding,
-      resourceType,
+    url,
+    contentType,
+    compressedBytes,
+    uncompressedBytes,
+    bytes,
+    encoding: contentEncoding,
+    resourceType,
   }
 };
-
-// db constants
-
-const DB = 'emissionsDB';
-const STORE = 'emissions';
 
 const openDatabase = async () => {
   return new Promise((resolve, reject) => {
@@ -42,7 +220,7 @@ const openDatabase = async () => {
       const db = event.target.result;
       if (!db.objectStoreNames.contains(STORE)) {
         // Use auto generated id` as the record key
-        db.createObjectStore(STORE, { keyPath: "id", autoIncrement:true });
+        db.createObjectStore(STORE, { keyPath: 'id', autoIncrement: true });
       }
     };
 
@@ -56,29 +234,20 @@ const openDatabase = async () => {
   })
 };
 
-const saveNetworkTraffic = async (requestResponse) => {
+const saveNetworkTraffic = async (responseDetails) => {
   const db = await openDatabase();
   const tx = db.transaction(STORE, 'readwrite');
   const emissions = tx.objectStore(STORE);
 
-  // Query the db for an entry with the key of the new record we want to add
-  const request = emissions.get(requestResponse.url);
-
-  request.onsuccess = (event) => {
-    // There is no entry for this key, so we create a new record
-    if (!event.target.result) {
-      emissions.add(requestResponse);
-    }
-  };
-  request.onerror = (e) => {
-    // There was an error checking for an existing record
-    console.log(e);
+  const record = {
+    url: responseDetails.url,
+    bytes: responseDetails.bytes,
+    contentType: responseDetails.contentType,
+    resourceType: responseDetails.resourceType,
   };
 
-  await new Promise((resolve, reject) => {
-    tx.oncomplete = resolve;
-    tx.onerror = tx.onabort = reject;
-  });
+  await emissions.add(record);
+
   db.close();
 };
 
@@ -87,9 +256,15 @@ const fetchHandler = (event) => {
     (async () => {
       try {
         const response = await fetch(event.request);
-        const responseDetails = getResponseDetails(response);
+        const clonedResponse = response.clone();
+        const responseDetails = await getResponseDetails(
+          clonedResponse,
+          'browser'
+        );
 
-        saveNetworkTraffic(responseDetails);
+        if (responseDetails) {
+          await saveNetworkTraffic(responseDetails);
+        }
 
         return response
       } catch (e) {
